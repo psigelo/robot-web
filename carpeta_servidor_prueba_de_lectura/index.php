0b77afb85f1280fa0d441c4729baf2bee01eb72f
Warren Eaton <warren.eaton@alumnos.usm.cl>
2013-09-15 14:28:17 -0300

    general update

--- carpeta_servidor_prueba_de_lectura/index.php
+++ carpeta_servidor_prueba_de_lectura/index.php
@@ -10,7 +10,7 @@
 		var xmlhttp = ConstructorXMLHttpRequest();		
 
 		// Datos generales del robot (valores temporales de prueba)
-		var numMotores = 6;
+		var numMotores = 3;
 		var anchoDivisiones = 240/numMotores;
 		var anchoBarras = anchoDivisiones/2; 
 		var offset = anchoBarras/2;
@@ -25,22 +25,35 @@
 				xmlhttp.onreadystatechange = function() {
 					if(xmlhttp.readyState == 4)
 					{
+						// Necesario para recibir la respuesta en formato XML
 						if (xmlhttp.overrideMimeType) xmlhttp.overrideMimeType('text/xml');
 
+						// Verifica si la sintaxis de la respuesta está en XML, sino retorna nulo
 						var xmlDoc = xmlhttp.responseXML;
 
+						// Genera un objeto DOM a partir de la respuesta interpretada como XML
 					    if (!xmlDoc) xmlDoc = (new DOMParser()).parseFromString(xmlhttp.responseText, 'text/xml');
-					    
-					    var tempMotores = xmlDoc.getElementsByTagName('motor');
-						
-						var canvas = document.getElementById('temperatura');
+
+					    var children = xmlDoc.getElementsByTagName('child');
+
+					    // Especifica el objeto CANVAS a utilizar
+						var canvas = document.getElementById(archivo);
 
 						if (canvas.getContext){
+							
     						var ctx = canvas.getContext('2d');
+    						var temperatura;
+
 					        for (var i = 0; i < numMotores; i++) {
-					        	ctx.fillStyle = "#BF5000"; // color temporal
+					        	temperatura = children[i].childNodes[0].nodeValue;
+
+					        	//hexString = temperatura.toString(16) + (90 - temperatura).toString(16);
+					        	
+					        	ctx.fillStyle = 'rgb(' + temperatura*(51/18) + ',' + (255 - temperatura*(51/18)) + ',00)'; 
 						       	ctx.fillRect (20, 110 + i*anchoDivisiones + offset, 112, anchoBarras);
-						       	ctx.clearRect(20, 110 + i*anchoDivisiones + offset, 112*(1 - (tempMotores[i].childNodes[0].nodeValue)/90), anchoBarras);
+						       	ctx.clearRect (20, 110 + i*anchoDivisiones + offset, 112*(1 - temperatura/90), anchoBarras);
+
+						       	//document.getElementById('resultado').innerHTML = 'rgb(' + temperatura*(51/18) + ',' + (255 - temperatura*(51/18)) + ',00)'; 
 					        };
 				    	}
 				    	else alert('Error: No se pudo obtener el contexto [canvas.getContext()]');
@@ -53,7 +66,8 @@
 		}
 
 		// Loop infinito cada 1000ms = 1s
-		var temp = setInterval(function(){Coger('temperatura')}, 1000); 
+		var temp = setInterval(function(){Coger('temperatura')}, 1000);
+		//var temp = setInterval(function(){Coger('corriente')}, 1000); 
 		
 
 	</script>
--- carpeta_servidor_prueba_de_lectura/lector.php
+++ carpeta_servidor_prueba_de_lectura/lector.php
@@ -9,10 +9,10 @@
 			$temp = explode("	", $line);	
 			fclose($file);
 			echo '<?xml version="1.0" ?>';
-			echo '<motores>';
+			echo '<root>';
 			for ( $i = 0 ; $i < count($temp) ; $i++)
-				echo '<motor id="', $i, '">', $temp[$i], '</motor>';
-			echo '</motores>';
+				echo '<child id="', $i, '">', $temp[$i], '</child>';
+			echo '</root>';
 		}
 		else {
 			echo 'Error: No se pudo cargar archivo <i>', $archivo, '</i>';		
